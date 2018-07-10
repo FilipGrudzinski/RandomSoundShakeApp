03aa5d63e8296dc35a705673d43738934c93e2cf
FilipGrudzinski <grudzinski.filip@gmail.com>
2018-07-10 18:38:47 +0200

    Testing without sounds

--- RandomSoundShakeApp/ViewController.swift
+++ RandomSoundShakeApp/ViewController.swift
@@ -7,14 +7,43 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func motionBegan(_ motion: UIEventSubtype, with event: UIEvent?) {
+        
+        if event?.subtype == UIEventSubtype.motionShake {
+            
+            let soundsArray = ["bomb", "splash" , "cow", "fart", "kat", "dog"]
+            let random = Int(arc4random_uniform(UInt32(soundsArray.count)))
+            
+            let fileToPlay = Bundle.main.path(forResource: soundsArray[random], ofType: "mp3")
+            
+            do {
+                
+                //try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: fileToPlay!))
+                //player.play()
+                print(soundsArray[random]) // Testing without sounds
+                
+            } catch {
+            
+                print(error)
+            
+            }
+            
+        }
+        
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
